030a8c46c9a000c761851c3c93a01a7c9bb2c5e0
dbohtess <sultan.dbohtes@gmail.com>
2026-07-18 18:04:12 +0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sultan.nekoringarage',
+  appName: 'Nekorin Garage',
+  webDir: 'dist'
+};
+
+export default config;
