049ec7211d0e3a91e38f2a81c301820d68f14a71
dbohtess <sultan.dbohtes@gmail.com>
2026-07-25 15:02:51 +0400

    Configure Vite for repository hosting

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Nekorin-Garage-V1/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
